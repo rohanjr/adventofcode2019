49f357ac21ce0a0784c2215663b68cab44b9fe39
Rohan Jacob-Rao <rohanjr@gmail.com>
2019-12-09 20:25:54 -0500

    Remove unnecessary IO in Day 7

--- 7/a/Main.hs
+++ 7/a/Main.hs
@@ -3,38 +3,37 @@
 module Main where
 
 import Data.List
+import Data.Array
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
-import Data.Array
-import Control.Monad
 
 main :: IO ()
 main = do
   prog <- T.getLine
   let strs = T.split (== ',') prog
       nums :: [Int] = map (read . T.unpack) strs
-  maxOutput <- findOptimal nums
+      maxOutput = findOptimal nums
   putStrLn $ show maxOutput
 
-findOptimal :: [Int] -> IO Int
-findOptimal prog = do
+findOptimal :: [Int] -> Int
+findOptimal prog =
   let len = length prog
       progArray = listArray (1, len) prog
       phaseSeqs = permutations [0..4]
-  outputs <- mapM (\phaseSeq -> runPhases phaseSeq progArray) phaseSeqs
-  return $ maximum outputs
+      outputs = map (\phaseSeq -> runPhases phaseSeq progArray) phaseSeqs
+  in maximum outputs
 
-runPhases :: [Int] -> Array Int Int -> IO Int
+runPhases :: [Int] -> Array Int Int -> Int
 runPhases phaseSeq prog =
-  foldM runWithPhase 0 phaseSeq
+  foldl' runWithPhase 0 phaseSeq
  where
-  runWithPhase :: Int -> Int -> IO Int
-  runWithPhase input phase = do
-    (_, outputs) <- run [phase, input] 1 prog
-    return $ head outputs -- assume exactly one output per run
+  runWithPhase :: Int -> Int -> Int
+  runWithPhase input phase =
+    let (_, outputs) = run [phase, input] 1 prog
+    in head outputs -- assume exactly one output per run
 
 -- Return new array storage and outputs so far
-run :: [Int] -> Int -> Array Int Int -> IO (Array Int Int, [Int])
+run :: [Int] -> Int -> Array Int Int -> (Array Int Int, [Int])
 run inputs i a =
   let (opCode, paramModes) = splitInstr (a ! i)
   in case opCode of
@@ -46,10 +45,10 @@ run inputs i a =
       6 -> jump paramModes (== 0)
       7 -> binaryOp paramModes (\x y -> if x < y then 1 else 0)
       8 -> binaryOp paramModes (\x y -> if x == y then 1 else 0)
-      99 -> return (a, [])
+      99 -> (a, [])
       _ -> error "Invalid program"
  where
-  binaryOp :: [Int] -> (Int -> Int -> Int) -> IO (Array Int Int, [Int])
+  binaryOp :: [Int] -> (Int -> Int -> Int) -> (Array Int Int, [Int])
   binaryOp (mode1 : mode2 : _) op =
     let pos1 = a ! (i + 1)
         pos2 = a ! (i + 2)
@@ -61,22 +60,22 @@ run inputs i a =
     in run inputs (i + 4) newA
   binaryOp _ _ = error "No parameter modes passed to binary operation"
 
-  storeInput :: IO (Array Int Int, [Int])
+  storeInput :: (Array Int Int, [Int])
   storeInput =
     case inputs of
       [] -> error "No input value to store"
       first : rest ->
         run rest (i + 2) $ a // [((a ! (i + 1)) + 1, first)]
 
-  output :: [Int] -> IO (Array Int Int, [Int])
-  output (mode : _) = do
+  output :: [Int] -> (Array Int Int, [Int])
+  output (mode : _) =
     let pos = a ! (i + 1)
         val = if mode == 0 then a ! (pos + 1) else pos
-    (newA, outputs) <- run inputs (i + 2) a
-    return (newA, val : outputs)
+        (newA, outputs) = run inputs (i + 2) a
+    in (newA, val : outputs)
   output _ = error "No parameter code for output instruction"
 
-  jump :: [Int] -> (Int -> Bool) -> IO (Array Int Int, [Int])
+  jump :: [Int] -> (Int -> Bool) -> (Array Int Int, [Int])
   jump (mode1 : mode2 : _) cond =
     let pos1 = a ! (i + 1)
         pos2 = a ! (i + 2)
--- 7/b/Main.hs
+++ 7/b/Main.hs
@@ -3,53 +3,52 @@
 module Main where
 
 import Data.List
+import Data.Array
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
-import Data.Array
-import Control.Monad
 
 main :: IO ()
 main = do
   prog <- T.getLine
   let strs = T.split (== ',') prog
       nums :: [Int] = map (read . T.unpack) strs
-  maxOutput <- findOptimal nums
+      maxOutput = findOptimal nums
   putStrLn $ show maxOutput
 
-findOptimal :: [Int] -> IO Int
-findOptimal prog = do
+findOptimal :: [Int] -> Int
+findOptimal prog =
   let len = length prog
       progs = listArray (1, 5) $ replicate 5 $ listArray (1, len) prog
       ips = listArray (1, 5) $ replicate 5 1
       phaseSeqs = permutations [5..9]
-  outputs <- mapM (\phaseSeq -> runLoop phaseSeq [0] 1 ips progs) phaseSeqs
-  return $ maximum outputs
+      outputs = map (\phaseSeq -> runLoop phaseSeq [0] 1 ips progs) phaseSeqs
+  in maximum outputs
 
 data Status = Finished | Pending Int
 
-runLoop :: [Int] -> [Int] -> Int -> Array Int Int -> Array Int (Array Int Int) -> IO Int
-runLoop phaseSeq inputs amp ips progs = do
+runLoop :: [Int] -> [Int] -> Int -> Array Int Int -> Array Int (Array Int Int) -> Int
+runLoop phaseSeq inputs amp ips progs =
   let (inputs', phaseSeq') =
         case phaseSeq of
           [] -> (inputs, [])
           phase : rest -> (phase : inputs, rest)
-  (newProg, outputs, status) <- run inputs' (ips ! amp) (progs ! amp)
-  case status of
-    Finished
-      | amp == length progs
-      -- Circuit finished, so return final output (assuming exactly one)
-      -> return (head outputs)
-      | otherwise
-      -- This amp has halted, but we need to finish the round
-      -> runLoop phaseSeq' outputs (amp + 1) ips progs
-    Pending i ->
-      let amp' = if amp == length progs then 1 else amp + 1
-          ips' = ips // [(amp, i)]
-          progs' = progs // [(amp, newProg)]
-      in runLoop phaseSeq' outputs amp' ips' progs'
+      (newProg, outputs, status) = run inputs' (ips ! amp) (progs ! amp)
+  in case status of
+      Finished
+        | amp == length progs
+        -- Circuit finished, so return final output (assuming exactly one)
+        -> head outputs
+        | otherwise
+        -- This amp has halted, but we need to finish the round
+        -> runLoop phaseSeq' outputs (amp + 1) ips progs
+      Pending i ->
+        let amp' = if amp == length progs then 1 else amp + 1
+            ips' = ips // [(amp, i)]
+            progs' = progs // [(amp, newProg)]
+        in runLoop phaseSeq' outputs amp' ips' progs'
 
 -- Return new array storage, outputs so far and progress status
-run :: [Int] -> Int -> Array Int Int -> IO (Array Int Int, [Int], Status)
+run :: [Int] -> Int -> Array Int Int -> (Array Int Int, [Int], Status)
 run inputs i a =
   let (opCode, paramModes) = splitInstr (a ! i)
   in case opCode of
@@ -61,10 +60,10 @@ run inputs i a =
       6 -> jump paramModes (== 0)
       7 -> binaryOp paramModes (\x y -> if x < y then 1 else 0)
       8 -> binaryOp paramModes (\x y -> if x == y then 1 else 0)
-      99 -> return (a, [], Finished)
+      99 -> (a, [], Finished)
       _ -> error "Invalid program"
  where
-  binaryOp :: [Int] -> (Int -> Int -> Int) -> IO (Array Int Int, [Int], Status)
+  binaryOp :: [Int] -> (Int -> Int -> Int) -> (Array Int Int, [Int], Status)
   binaryOp (mode1 : mode2 : _) op =
     let pos1 = a ! (i + 1)
         pos2 = a ! (i + 2)
@@ -76,22 +75,22 @@ run inputs i a =
     in run inputs (i + 4) newA
   binaryOp _ _ = error "No parameter modes passed to binary operation"
 
-  storeInput :: IO (Array Int Int, [Int], Status)
+  storeInput :: (Array Int Int, [Int], Status)
   storeInput =
     case inputs of
-      [] -> return (a, [], Pending i)
+      [] -> (a, [], Pending i)
       first : rest ->
         run rest (i + 2) $ a // [((a ! (i + 1)) + 1, first)]
 
-  output :: [Int] -> IO (Array Int Int, [Int], Status)
-  output (mode : _) = do
+  output :: [Int] -> (Array Int Int, [Int], Status)
+  output (mode : _) =
     let pos = a ! (i + 1)
         val = if mode == 0 then a ! (pos + 1) else pos
-    (newA, outputs, status) <- run inputs (i + 2) a
-    return (newA, val : outputs, status)
+        (newA, outputs, status) = run inputs (i + 2) a
+    in (newA, val : outputs, status)
   output _ = error "No parameter code for output instruction"
 
-  jump :: [Int] -> (Int -> Bool) -> IO (Array Int Int, [Int], Status)
+  jump :: [Int] -> (Int -> Bool) -> (Array Int Int, [Int], Status)
   jump (mode1 : mode2 : _) cond =
     let pos1 = a ! (i + 1)
         pos2 = a ! (i + 2)
